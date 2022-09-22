de3db8b3ef551b9cb9ecbe1aa79f08785345cfd5
Daniel Chaparro <daniel.marin92@hotmail.com>
2022-09-21 23:25:02 -0500

    .

--- EjerciciosDdApps/Sep-13-2022/BinarioADecimal.dart
+++ EjerciciosDdApps/Sep-13-2022/BinarioADecimal.dart
@@ -1,5 +1,34 @@
 import 'dart:io';
+import 'dart:math';
 void main(List<String> args) {
-  print("Introduce un numero binario: ");
-  int binario = int.parse(stdin.readByteSync().toString());
+  print("Binario a decimal\nnumero binario: ");
+  String binario = stdin.readLineSync().toString(); // Entrada de datos binarios
+
+  print( '$binario => ${transformarBinarioADecimal( binario )}' );
 }
+
+int transformarBinarioADecimal( String binario ) {
+  int decimal = 0;
+  int i = binario.length - 1;
+  int aux = 0;
+
+  while( i >= 0 ) {
+    // Validar el numero binario por posicion
+    // Suma el valor binario equivalente a la posicion donde se encuentra un 1
+    // decimal += binario[i] == "1" ? obtenerPotencia( i ) : 0;
+    decimal += binario[i] == "1" ? pow(2, aux).toInt() : 0;
+
+    print( '${binario[i]} 2^$aux = ${pow(2, aux).toInt()}' );
+
+    aux++;
+    i--;
+  }
+
+  return decimal;
+}
+
+// int obtenerPotencia(int index) {
+//   int potencia = 1;
+//   for ( int i = 0; i < index; i++ ) { potencia *= 2; }
+//   return potencia;
+// }
---
+++ EjerciciosDdApps/Sep-13-2022/DecimalABinario.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+void main(List<String> args) {
+  print('Decimal a Binario\nnumero decimal: ');
+  int decimal = int.parse( stdin.readLineSync().toString() );
+
+  print('$decimal => ${convertirDecimalABinario( decimal )}');
+}
+
+int convertirDecimalABinario( int decimal ) {
+  String binario = '';  
+
+  do {
+    binario += decimal%2 > 0 ? '0' : '1';
+    print('$decimal / 2 = ${(decimal / 2).truncate()} -> $binario');
+    decimal = (decimal / 2).truncate();
+  } while( decimal != 0 );
+
+  return int.parse(binario);
+}
